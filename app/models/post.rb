88023e970eb69c533321a2a155c6c4f39eaf6a08
Jared Gray <jd.gray@yahoo.com>
2015-07-21 16:51:16 -0700

    reformating, validation testing

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,8 @@
 class Post < ActiveRecord::Base
   extend FriendlyId
   friendly_id :title, use: :slugged
+
+  validates :title, presence: true
+  validates :content, presence: true
+
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,9 @@
 class Project < ActiveRecord::Base
   extend FriendlyId
   friendly_id :title, use: :slugged
+
+  validates :title,       presence: true
+  validates :description, presence: true
+  validates :link,        presence: true
+
 end
