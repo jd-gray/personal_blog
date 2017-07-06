a43e532e07c3b6230f176215d61b5e4a53578ae1
Jared Gray <jared@jaredgray.us>
2017-07-05 19:20:26 -0700

    Remove unused users table

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,10 @@
 Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
-  devise_for :users
 
   resources :posts, only: [:show]
   resources :projects
 
-  get 'welcome/index'
   root 'welcome#index'
 
   get '*path' => redirect('/')
