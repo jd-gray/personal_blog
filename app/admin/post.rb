b846d1b83fe7dbf00859ddd20fa4419b92e21273
Jared Gray <jared@jaredgray.us>
2017-07-23 21:09:57 -0700

    AA find by friendly id and truncate post content length

--- app/admin/post.rb
+++ app/admin/post.rb
@@ -1,6 +1,17 @@
 ActiveAdmin.register Post do
   permit_params :title, :content
 
+  index do
+    column :id
+    column :title
+    column :content do |post|
+      truncate(post.content, omision: "...", length: 100)
+    end
+    column :created_at
+    
+    actions
+  end
+
   form do |f|
     f.inputs "Create Post" do
       f.input :title
