56c1308086d6613e25b05b26c2b04e9a25df27d5
Jared Gray <jared@jaredgray.us>
2017-07-24 20:58:44 -0700

    Always order by desc

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,6 @@
 class WelcomeController < ApplicationController
   def index
-    @posts = params[:tag] ? Post.tagged_with(params[:tag]) : Post.all.order('created_at desc')
-    @posts = @posts.paginate(page: params[:page], per_page: 5).decorate
+    @posts = params[:tag] ? Post.tagged_with(params[:tag]) : Post.all
+    @posts = @posts.order('created_at desc').paginate(page: params[:page], per_page: 5).decorate
   end
 end
