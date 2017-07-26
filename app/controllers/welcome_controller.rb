bf9b452390e5dbcbc9b532451bcf18f2e8b06562
Jared Gray <jared@jaredgray.us>
2017-07-25 20:44:26 -0700

    Display all badges in side view

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,5 +2,7 @@ class WelcomeController < ApplicationController
   def index
     @posts = params[:tag] ? Post.tagged_with(params[:tag]) : Post.all
     @posts = @posts.order('created_at desc').paginate(page: params[:page], per_page: 5).decorate
+
+    @tags = ActsAsTaggableOn::Tag.all
   end
 end
