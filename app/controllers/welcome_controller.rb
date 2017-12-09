a91cc90cfa684bc4648d47cf3ac6735e8f81f95e
Jared Gray <jared@jaredgray.us>
2017-12-08 23:16:35 -0700

    More fun with codeclimate

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,9 +2,15 @@
 
 class WelcomeController < ApplicationController
   def index
-    @posts = params[:tag] ? Post.tagged_with(params[:tag]) : Post.all
-    @posts = @posts.desc.paginate(page: params[:page], per_page: 5).decorate
+    posts = welcome_params ? Post.tagged_with(welcome_params) : Post.all
+    @posts = posts.desc.paginate(page: params[:page], per_page: 5).decorate
 
     @tags = ActsAsTaggableOn::Tag.all
+  end
+
+  private
+
+  def welcome_params
+    params[:tag]
   end
 end
